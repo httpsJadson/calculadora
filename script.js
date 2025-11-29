96fd2d192fd1b97cba4eb267074eaa7f8aace4a0
httpsJadson <dev.jadson27@gmail.com>
2025-11-29 16:19:04 -0300

    Melhoria nos html da Calculadora

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function criaCalculadora(){
 
       //metodos
       inicia(){
+        this.criaBotoes();
         this.cliqueBotoes();
         this.pressionaEnter();
       },
@@ -32,7 +33,42 @@ function criaCalculadora(){
         }catch(e){alert('Conta Inválida')}
           
       },
-
+      criaBotoes(){
+        const divBotoes = document.querySelector('.botoes');
+        divBotoes.innerHTML = `
+            <div class="linha" id="linha-1">
+                <button class="btn btn-clear">C</button>
+                <button class="btn btn-num">(</button>
+                <button class="btn btn-num">)</button>
+                <button class="btn btn-num operador"> + </button>
+            </div>
+            <div class="linha" id="linha-2">
+                <button class="btn btn-num">7</button>
+                <button class="btn btn-num">8</button>
+                <button class="btn btn-num">9</button>
+                <button class="btn btn-num operador"> - </button>
+            </div>
+            <div class="linha" id="linha-3">
+                <button class="btn btn-num">4</button>
+                <button class="btn btn-num">5</button>
+                <button class="btn btn-num">6</button>
+                <button class="btn btn-num operador"> * </button>
+            </div>
+            <div class="linha" id="linha-4">
+                <button class="btn btn-num">1</button>
+                <button class="btn btn-num">2</button>
+                <button class="btn btn-num">3</button>
+                <button class="btn btn-num operador"> / </button> 
+            </div>
+            <div class="linha" id="linha-5">
+                <button class="btn btn-num">.</button>
+                <button class="btn btn-num">0</button>  
+                <button class="btn btn-del">&laquo;</button>
+                <button class="btn btn-eq operador">=</button>
+            </div>
+        
+        `
+      },
       pressionaEnter(){
         this.display.addEventListener('keyup', e =>{
           if(e.keyCode === 13){
